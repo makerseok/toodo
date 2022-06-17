7e75275e5cc14d654a5fc568299c763c3575dd88
makerseok <maker7788@gmail.com>
2022-06-17 12:50:45 +0900

    refactor: typescript 템플릿으로 변경
    CRA 실행할 때 --template typescript 옵션을 추가해 js 파일 ts로 변경

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,10 @@
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">test!</header>
     </div>
   );
 }

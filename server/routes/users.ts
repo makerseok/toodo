92261c2887b735f334fb280e383f65e129958abf
makerseok <maker7788@gmail.com>
2022-06-19 01:59:27 +0900

    feature(users.ts): 로그인 관련 테스트용 코드 추가
    사용자가 입력한 email, password 비교 후 결과를 화면에 표시

--- server/routes/users.ts
+++ server/routes/users.ts
@@ -5,6 +5,26 @@ import { ILogin, ITypedRequestBody, IUser } from '../types/IUser';
 const usersRouter = express.Router();
 
 usersRouter.post(
+  '/login',
+  async (req: ITypedRequestBody<ILogin>, res: express.Response) => {
+    const user = await User.findByEmail(req.body?.email);
+    if (user) {
+      if (req.body?.password) {
+        if (await user.checkPassword(req.body?.password)) {
+          res.send('login!');
+        } else {
+          res.send('wrong password!');
+        }
+      } else {
+        res.send('please input password');
+      }
+    } else {
+      res.send('email not exist');
+    }
+  },
+);
+
+usersRouter.post(
   '/register',
   async (req: ITypedRequestBody<IUser>, res: express.Response) => {
     console.log(req.body);

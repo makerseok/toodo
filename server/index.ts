c48c66587693733c984b3f64fe977aec33636bd5
makerseok <maker7788@gmail.com>
2022-06-19 01:46:48 +0900

    feature(index.ts): mongoDB 연결
    mongoose를 사용해 mongoDB와 연결

--- server/index.ts
+++ server/index.ts
@@ -1,8 +1,24 @@
-import express, { NextFunction, Request, Response } from 'express';
+import express, {
+  json,
+  NextFunction,
+  Request,
+  Response,
+  urlencoded,
+} from 'express';
+import mongoose from 'mongoose';
+import { MONGO_URL } from './config/dev';
 
 const app = express();
 
 const port = '5000';
+
+app.use(urlencoded({ extended: true }));
+app.use(json());
+
+mongoose
+  .connect(MONGO_URL)
+  .then(() => console.log('mongoDB Connected...'))
+  .catch(err => console.log(err));
 
 app.get('/api/welcome', (req: Request, res: Response, next: NextFunction) => {
   res.send('welcome!');

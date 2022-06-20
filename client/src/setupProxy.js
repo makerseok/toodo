9256ab4328e0639b91dde9fcf540f15e901799d7
makerseok <maker7788@gmail.com>
2022-06-20 20:17:15 +0900

    feature: proxy 설정 추가
    /api로 접근하는 모든 요청의 포트를 5000번으로 변경하는 proxy 설정 추가

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,6 +1,6 @@
-import { createProxyMiddleware } from 'http-proxy-middleware';
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
-export default function createProxy(app) {
+module.exports = app => {
   app.use(
     '/api',
     createProxyMiddleware({
@@ -8,4 +8,4 @@ export default function createProxy(app) {
       changeOrigin: true,
     }),
   );
-}
+};

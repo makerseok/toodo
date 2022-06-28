06670981c6770b6d078e289befcb9002ae8b4585
makerseok <maker7788@gmail.com>
2022-06-28 09:01:39 +0900

    feature: Label, Input 정렬

--- client/src/components/Input.tsx
+++ client/src/components/Input.tsx
@@ -10,17 +10,24 @@ interface IInput {
 
 const StyledLabel = styled.label`
   display: inline-block;
+  width: 25%;
+  text-align: right;
 `;
 
 const StyledInput = styled.input`
   display: inline-block;
+  display: block;
+  width: 70%;
+  /* width: 400px; */
+  height: 30px;
+  margin: 5px;
 `;
 
 const StyledDiv = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
-  width: 50vw;
+  width: 30vw;
 `;
 
 function Input({ id, label, type, onChange }: IInput) {
